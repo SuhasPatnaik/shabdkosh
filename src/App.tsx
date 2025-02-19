07766f7460b8922ac9a85fa8c30f64a1cbf7b064
Suhas Patnaik <suhaspatnaik111@gmail.com>
2025-02-19 23:59:59 +0530

    Project setup with Tailwind, Shadcn, Theme Provider for light-dark modes

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import { ModeToggle } from "./components/mode-toggle";
+
+function App() {
+  return <ModeToggle />;
+}
+
+export default App;
---
+++ src/components/mode-toggle.tsx
@@ -0,0 +1,19 @@
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "./theme-provider";
+import { Switch } from "./ui/switch";
+
+export function ModeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <div className="flex items-center gap-2">
+      <Switch
+        id="theme-switch"
+        checked={theme === "dark"}
+        onCheckedChange={(checked) => setTheme(checked ? "dark" : "light")}
+        className="w-[2.5rem] data-[state=checked]:bg-primary-accent data-[state=unchecked]:bg-slate-500"
+      />
+      <Moon className="size-6 text-slate-500 dark:text-primary-accent" />
+    </div>
+  );
+}
